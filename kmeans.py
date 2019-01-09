f6fdf37521351fce762758d66ce6a2343c8469cb
yumworm <42192687+yumworm@users.noreply.github.com>
2019-01-08 18:18:16 -0800

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -9,7 +9,7 @@ import os
 
 
 if __name__ == '__main__':
-    feats, labels = load_feat_db()
+    feats, color_feats, labels = load_feat_db()
     model = KMeans(n_clusters=N_CLUSTERS, random_state=0, n_jobs=-1).fit(feats)
     model_path = os.path.join(DATASET_BASE, r'models', r'kmeans.m')
     joblib.dump(model, model_path)
